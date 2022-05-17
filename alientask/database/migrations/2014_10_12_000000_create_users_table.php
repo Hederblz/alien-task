36cf88dc22f63dee1519a7288294b347878c00f8
CaioSousa32 <caio123c@gmail.com>
2022-05-17 14:29:18 -0300

    GH-12 Criando a migration usuario

--- alientask/database/migrations/2014_10_12_000000_create_users_table.php
+++ alientask/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('cpf');
+            $table->integer('telefone');
             $table->rememberToken();
             $table->timestamps();
         });
