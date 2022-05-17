29114a1c949d29c61f0c2180ce64e54a8fef8977
jonatas2021 <jonatascandido0@gmail.com>
2022-05-17 14:45:20 -0300

    GH-11 Editando o modelo de usuario

--- alientask/app/Models/User.php
+++ alientask/app/Models/User.php
@@ -18,11 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nome',
         'email',
         'password',
         'cpf',
-        'telephone'
+        'telepone'
     ];
 
     /**
