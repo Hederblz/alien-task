fd76ed87d4eb35a4ff734987128f0e9e633f835c
CaioSousa32 <caio123c@gmail.com>
2022-05-17 15:47:20 -0300

    GH-20 Criando a migration historico

---
+++ alientask/database/migrations/2022_05_17_184258_create_historico_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoricoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historico', function (Blueprint $table) {
+            $table->id();
+            $table->string('acao');
+            $table->foreignId('tarefa');
+            $table->dateTime('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historico');
+    }
+}
