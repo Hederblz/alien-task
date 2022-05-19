26cd75d51609127f6dac2f2e9a8c0a53a8c37b4b
jonatas2021 <jonatascandido0@gmail.com>
2022-05-18 21:10:49 -0300

    GH-24 Criando o modelo de marcador_has_tarefa

---
+++ alientask/app/Models/Marcador_has_Tarefa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marcador_has_Tarefa extends Model
+{
+    use HasFactory;
+    protected $fillable[
+        'Marcador_id',
+        'Tarefa_id'
+    ];
+}
