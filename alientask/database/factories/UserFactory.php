737daae6698524e002cd133e48842405e96c1b68
jonatas2021 <jonatascandido0@gmail.com>
2022-05-18 21:45:03 -0300

    GH-25 Corrigindo os erros do Userfactory

--- alientask/database/factories/UserFactory.php
+++ alientask/database/factories/UserFactory.php
@@ -20,8 +20,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'cpf' => $this->fake->cpf(),
-            'telefone'=> $this->fake->cellphoneNumber(),
+            'cpf' => $this->faker->cpf(),
+            'telefone'=> $this->faker->cellphoneNumber(),
         ];
     }
 
