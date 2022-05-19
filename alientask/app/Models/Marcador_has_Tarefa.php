c6fecc75604af9614354bda3ea66d301e74668fe
jonatas2021 <jonatascandido0@gmail.com>
2022-05-19 14:24:24 -0300

    GH-24 Editando o modelo_has_tarefa

--- alientask/app/Models/Marcador_has_Tarefa.php
+++ alientask/app/Models/Marcador_has_Tarefa.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Marcador_has_Tarefa extends Model
 {
     use HasFactory;
-    protected $fillable[
+    protected $fillable = [ 
         'Marcador_id',
         'Tarefa_id'
     ];
