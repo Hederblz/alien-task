11fa187f923d6f3f10523da11a133a9f9037dc4b
Alanjoose <alanjsyt@gmail.com>
2022-05-20 08:57:57 -0300

    GH-29 Alteração dos seeders e instalação do doctrine/dbal ORM

--- alientask/database/factories/UserFactory.php
+++ alientask/database/factories/UserFactory.php
@@ -16,11 +16,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'nome' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             //  'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'password' => Hash::make('senhapadrao'),
+            'password' => Hash::make('senhapadrao'), //Usar essa senha para acessar as contas geradas pelo faker;
             'remember_token' => Str::random(10),
             'cpf' => $this->faker->cpf(),
             'telefone'=> $this->faker->cellphoneNumber(),
