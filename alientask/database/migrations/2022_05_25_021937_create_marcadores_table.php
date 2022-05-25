c32860836545905ad34c01494675f552fe5ddca1
5636b <mush.comushroomhead@hotmail.com>
2022-05-24 23:23:53 -0300

    GH-41 Criação de migration e model marcador.

---
+++ alientask/database/migrations/2022_05_25_021937_create_marcadores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marcadores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo');
+            $table->string('cor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marcadores');
+    }
+};
