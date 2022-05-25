131dd245081e7cb18cd450510341cab11053c42d
jonatas2021 <jonatascandido0@gmail.com>
2022-05-25 20:54:50 -0300

    GH-49 Referenciando o seeder de notas

--- alientask/database/seeders/DatabaseSeeder.php
+++ alientask/database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             TarefaSeeder::class,
-            MarcadorSeeder::class
+            MarcadorSeeder::class,
+            NotaSeeder::class
         ]);
     }
 }
