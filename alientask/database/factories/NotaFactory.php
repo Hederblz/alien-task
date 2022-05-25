b2cf0cbeabf9e200837290acb8ea5d251b890ede
jonatas2021 <jonatascandido0@gmail.com>
2022-05-25 20:12:43 -0300

    GH-46 Criando factory de notas

---
+++ alientask/database/factories/NotaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Nota>
+ */
+class NotaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'titulo' => $this->faker->sentence(),
+            'conteudo' => $this->faker->text(),
+            'user_id' => User::all()->random()->id
+        ];
+    }
+}
