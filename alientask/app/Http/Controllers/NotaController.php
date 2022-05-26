afc2abac7f5deb23203e6587c748504431e0232a
jonatas2021 <jonatascandido0@gmail.com>
2022-05-25 21:42:37 -0300

    GH-51 Criando o controller de notas

---
+++ alientask/app/Http/Controllers/NotaController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nota;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NotaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Nota::create([
+            'titulo' => $request->title,
+            'conteudo' => $request->conteudo,
+            'user_id' => Auth::user()->id
+    ]);
+        return redirect('/dashboard');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nota $nota)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nota $nota)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Nota $nota)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nota $nota)
+    {
+        //
+    }
+}
