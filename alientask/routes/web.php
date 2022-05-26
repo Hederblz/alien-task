fa648a996fb2eb13ef91b9be2416a9f96d5741b2
jonatas2021 <jonatascandido0@gmail.com>
2022-05-25 22:20:49 -0300

    GH-51 Adicioando as rotas

--- alientask/routes/web.php
+++ alientask/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NotaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,5 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::post('/add-nota',[NotaController::class,'store']);
 require __DIR__.'/auth.php';
