b5843787a603863c4c21870a0ce6802b9cbb9c16
jonatas2021 <jonatascandido0@gmail.com>
2022-05-25 22:16:34 -0300

    GH-51 Criando o html e o foreach de Notas

--- alientask/resources/views/dashboard.blade.php
+++ alientask/resources/views/dashboard.blade.php
@@ -9,7 +9,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <table>
+                        @foreach(Auth::user()-> notas as $nota)
+                        <tr>
+                            <td>{{$nota -> title}}</td>
+                            <td>{{$nota -> conteudo}}</td>
+                        </tr>
+                        @endforeach
+                        <form action="add-nota" method="POST">
+                            @csrf
+                            <input type="text" name="titulo" placeholder="titulo">
+                            <input type="text" name="conteudo" placeholder="conteudo">
+                            <input type="subimit" value ="add">
+                        </form>
+                    </table>
                 </div>
             </div>
         </div>
